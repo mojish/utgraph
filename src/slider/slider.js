a66f9de9055f0640f65368ee6889d014567693cd
Mojtaba Shahbazi <mojtabashahbazi1377@gmail.com>
2018-08-13 16:56:14 +0430

    home page template added

--- src/slider/slider.js
+++ src/slider/slider.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import EventImage from '../images/event.png';
 import './slider.css';
-
+// var Carousel = require('nuka-carousel');
+import 'nuka-carousel'
 
 export class Slider extends React.Component {
   render() {
     return (
+      // <Carousel
+      //   renderTopCenterControls={({ currentSlide }) => (
+      //     <div>Slide: {currentSlide}</div>
+      //   )}
+      //   renderCenterLeftControls={({ previousSlide }) => (
+      //     <button onClick={previousSlide}>Previous</button>
+      //   )}
+      //   renderCenterRightControls={({ nextSlide }) => (
+      //     <button onClick={nextSlide}>Next</button>
+      //   )}
+      // >
+      // </Carousel>
       <div class="slider">
-        213123
       </div>
     );
   }
